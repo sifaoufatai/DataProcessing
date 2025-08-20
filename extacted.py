c5bdcc522492f9f54a19a84bbd54b5a5cdad67de
sifaoufatai <sifaoufatai@gmail.com>
2025-08-20 13:58:00 +0200

    commit -m my extractor

--- extacted.py
+++ extacted.py
@@ -1,7 +1,8 @@
 import json
 from markitdown import MarkItDown
+import os
 
-
+from argparse import ArgumentParser
 def extract_sequence(pdf_file, output_dir):
     md = MarkItDown()
     result = md.convert(pdf_file)
@@ -28,16 +29,21 @@ def extract_sequence(pdf_file, output_dir):
         sequences.append(
             {'name': sequence_name, 'content': '\n'.join(sequence)})
 
-    json_file = pdf_file.replace('.pdf', '.json')
+    json_file = os.path.splitext(os.path.basename(pdf_file))[0] + '.json'
     json_file = os.path.join(output_dir, json_file)
     with open(json_file, 'w') as f:
         json.dump(sequences, f, indent=4)
+        print(f"Sequences extraites et enregistrées dans {json_file}")
 
     return sequences
 
-import os
+
 if __name__ == '__main__':
-    dir ="/home/INT/idrissou.f/PycharmProjects/RagProject/Extractor/pdf"
-    for file in os.listdir(dir):
+    parser = ArgumentParser(description="Extraction des séquences depuis des fichiers PDF")
+    parser.add_argument('--pdf_dir', type=str, required=True, help="Répertoire contenant les fichiers PDF")
+    parser.add_argument('--output_dir', type=str, required=True, help="Répertoire de sortie pour les fichiers JSON")
+    args = parser.parse_args()
+
+    for file in os.listdir(args.pdf_dir):
         if file.endswith(".pdf"):
-            extract_sequence(os.path.join(dir, file), "output_dir")
+             a=extract_sequence(os.path.join(args.pdf_dir, file), args.output_dir)
